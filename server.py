f0cff65adf14d7b558b0b11793ae4be101f8ae2c
Gayathri Roy <gayathriroy3@gmail.com>
2026-08-01 15:36:08 +0530

    fast api docs url updated

--- server.py
+++ server.py
@@ -51,8 +51,8 @@ orchestrator = Orchestrator()
 app = FastAPI(
     title="Trade Discipline API",
     version="0.1.0",
-    docs_url="/api/docs",
-    openapi_url="/api/openapi.json")
+    docs_url="/docs",
+    openapi_url="/openapi.json")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
